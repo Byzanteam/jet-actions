f30d540c5ea7fedf8f4f366ef0d248eab3a13f9b
vanppo <vanppo@hotmail.com>
2023-01-12 15:30:23 +0800

    feat: add namespace input (#50)

--- k8s-rollout/entrypoint.sh
+++ k8s-rollout/entrypoint.sh
@@ -10,7 +10,11 @@ chmod 600 $JET_DEPLOY_KEY_PATH
 # Deploy
 echo "🚧 Start to deploy"
 
-ssh $INPUT_DEPLOY_USER@$INPUT_DEPLOY_HOST -p $INPUT_DEPLOY_PORT -i $JET_DEPLOY_KEY_PATH -o StrictHostKeyChecking=no -T "kubectl set image ${INPUT_RESOURCE} ${INPUT_CONTAINER}=${INPUT_REGISTRY}/${INPUT_IMAGE}:${INPUT_TAG}"
+ssh $INPUT_DEPLOY_USER@$INPUT_DEPLOY_HOST \
+  -p $INPUT_DEPLOY_PORT \
+  -i $JET_DEPLOY_KEY_PATH \
+  -o StrictHostKeyChecking=no \
+  -T "kubectl --namespace=${INPUT_NAMESPACE} set image ${INPUT_RESOURCE} ${INPUT_CONTAINER}=${INPUT_REGISTRY}/${INPUT_IMAGE}:${INPUT_TAG}"
 
 # kubectl set image $resource $container=$registry/$image:$tag
 # kubectl set image deployments/workflow-jet-chart-airbase jet-chart-airbase=registry.ap-northeast-1.aliyuncs.com/jet/airbase_umbrella:alpha
