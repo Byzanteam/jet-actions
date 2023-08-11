892b45e4f0d1911c1285b4fc2a5339c913a441a3
YkeLit <keyang117@outlook.com>
2023-08-11 15:44:53 +0800

    feat: non-root 下运行次 actions

--- compose-upgrade/entrypoint.sh
+++ compose-upgrade/entrypoint.sh
@@ -13,12 +13,12 @@ echo "::group::🚧 The $INPUT_SERVICE_NAME service is undergoing an upgrade on 
 # Backup remote host docker-compose file
 echo "############### Backup remote host docker-compose file. ###############"
 ssh $INPUT_USER@$INPUT_HOST -p $INPUT_PORT -i $KEY_PATH -o StrictHostKeyChecking=no -Tq \
-	"cp $INPUT_DOCKER_COMPOSE_FILE_PATH $INPUT_DOCKER_COMPOSE_FILE_PATH.`date +%Y%m%d%H%M`.bak"
+	"sudo cp $INPUT_DOCKER_COMPOSE_FILE_PATH $INPUT_DOCKER_COMPOSE_FILE_PATH.`date +%Y%m%d%H%M`.bak"
 
 # Modify the remote host docker-compose file
 echo "############### Backup successful. Modify the remote host docker-compose file. ###############"
 # Read the docker compose file contents
-docker_compose_contents=$(ssh $INPUT_USER@$INPUT_HOST -p $INPUT_PORT -i $KEY_PATH -o StrictHostKeyChecking=no -Tq "cat $INPUT_DOCKER_COMPOSE_FILE_PATH")
+docker_compose_contents=$(ssh $INPUT_USER@$INPUT_HOST -p $INPUT_PORT -i $KEY_PATH -o StrictHostKeyChecking=no -Tq "sudo cat $INPUT_DOCKER_COMPOSE_FILE_PATH")
 
 # Check whether the result is empty. If no, exit the execution.
 if [ -z "$docker_compose_contents" ]; then
@@ -30,12 +30,12 @@ fi
 modified_contents=$(echo "$docker_compose_contents" | yq ".services.$INPUT_SERVICE_NAME.image=\"$INPUT_IMAGE\"")
 
 # The modified content is written back to the remote host's docker compose file.
-echo "$modified_contents" | ssh $INPUT_USER@$INPUT_HOST -p $INPUT_PORT -i $KEY_PATH -o StrictHostKeyChecking=no -Tq "tee $INPUT_DOCKER_COMPOSE_FILE_PATH"
+echo "$modified_contents" | ssh $INPUT_USER@$INPUT_HOST -p $INPUT_PORT -i $KEY_PATH -o StrictHostKeyChecking=no -Tq " sudo tee $INPUT_DOCKER_COMPOSE_FILE_PATH"
 
 # Upgrade application
 echo "############### Modify successful. Upgrade application. ###############"
 ssh $INPUT_USER@$INPUT_HOST -p $INPUT_PORT -i $KEY_PATH -o StrictHostKeyChecking=no -Tq \
-	"docker-compose -f $INPUT_DOCKER_COMPOSE_FILE_PATH up -d"
+	"sudo docker-compose -f $INPUT_DOCKER_COMPOSE_FILE_PATH up -d"
 
 echo "::endgroup::"
 
