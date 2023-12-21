7b5db21bbdfc0db24f608515297dc6884ac6c897
YkeLit <keyang117@outlook.com>
2023-12-21 16:26:59 +0800

    chore: javascript actions

---
+++ registry-login/src/context.ts
@@ -0,0 +1,27 @@
+import * as core from "@actions/core";
+
+export interface Inputs {
+  registryHostname: string;
+  registryNamespace: string;
+  username: string;
+  password: string;
+}
+
+export function getInputs(): Inputs {
+  const registryInfo = core.getInput("registry-info");
+  const [registryHostname, registryNamespace, username, password] =
+    registryInfo.split(" ");
+
+  return {
+    registryHostname,
+    registryNamespace,
+    username,
+    password,
+  };
+}
+
+export function setOutputs() {
+  const inputs: Inputs = getInputs();
+  core.setOutput("registry-hostname", inputs.registryHostname);
+  core.setOutput("registry-namespace", inputs.registryNamespace);
+}
--- registry-login/src/main.ts
+++ registry-login/src/main.ts
@@ -1,58 +1,17 @@
-import * as core from 'https://esm.sh/@actions/core';
-import * as actionsToolkit from 'https://esm.sh/@docker/actions-toolkit';
-import {Exec} from 'https://esm.sh/@docker/actions-toolkit/lib/exec';
+import * as actionsToolkit from "@docker/actions-toolkit";
 
-export interface Inputs {
-  registryHostname: string;
-  registryNamespace: string;
-  username: string;
-  password: string;
-}
+import * as context from "./context";
+import * as docker from "./docker";
 
-export function getInputs(): Inputs {
-  const registryInfo = Deno.env.get('registry-info');
-  const [registryHostname, registryNamespace, username, password] = registryInfo.split(" ");
-
-  return {
-    registryHostname,
-    registryNamespace,
-    username,
-    password
-  };
-}
-
-export async function login(registry: string, username: string, password: string): Promise<void> {
-	if (!username || !password) {
-		throw new Error('Username and password required');
-	}
-
-	const loginArgs: Array<string> = ['login', '--password-stdin'];
-	loginArgs.push('--username', username);
-	loginArgs.push(registry);
-
-	core.info(`Logging into ${registry}...`);
-	await Exec.getExecOutput('docker', loginArgs, {
-		ignoreReturnCode: true,
-    silent: true,
-    input: ((new TextDecoder()).decode(((new TextEncoder()).encode(password))))
-	}).then(res => {
-		if (res.stderr.length > 0 && res.exitCode != 0) {
-			throw new Error(res.stderr.trim());
-		}
-		core.info(`Login Succeeded!`);
-	});
-}
-const inputs: Inputs = getInputs();
-
-export function setOutputs() {
-	core.setOutput("registry-hostname", inputs.registryHostname);
-	core.setOutput("registry-namespace", inputs.registryNamespace);
-}
+const inputs: context.Inputs = context.getInputs();
 
 export async function main(): Promise<void> {
-	setOutputs();
-	await login(inputs.registryHostname, inputs.username, inputs.password);
+  context.setOutputs();
+  await docker.login(inputs.registryHostname, inputs.username, inputs.password);
 }
 
-actionsToolkit.run(main);
+async function post(): Promise<void> {
+  await docker.logout(inputs.registryHostname);
+}
 
+actionsToolkit.run(main, post);
