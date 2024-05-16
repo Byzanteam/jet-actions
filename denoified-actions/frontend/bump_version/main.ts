8110d9016c798c201faec19fc37d35e02beb1778
shaddollxz <shaddollxz@163.com>
2024-05-16 10:58:54 +0800

    fixup! feat: bump fe-project new version

--- denoified-actions/frontend/bump_version/main.ts
+++ denoified-actions/frontend/bump_version/main.ts
@@ -1,12 +1,13 @@
 #!/usr/bin/env -S deno run --allow-all
 
-// Bumps the version of a mix project and opens a PR
+// Bumps the version of a pnpm project and opens a PR
 // Usage: deno run --allow-all bump_version.ts
 // Environment variables:
 // - GH_TOKEN: the GitHub token
 // - GH_WORKFLOW_ACTOR: the GitHub username of the actor
+// - GH_REPOSITORY: the GitHub repository in the format "owner/repo"
 // - RELEASE_KIND: the kind of release to make (patch, minor, major)
-// - PACKAGE_FILE: the update app package.json file path
+// - PACKAGE_FILE: the package.json file path
 
 import { format, increment, parse, type ReleaseType } from "@std/semver";
 
@@ -18,10 +19,17 @@ import {
 } from "../../fundamental/utils.ts";
 import { default as $, Path } from "@david/dax";
 import { isMonorepoProject } from "../utils.ts";
+import { fileExists } from "../../fundamental/utils.ts";
 
 const actor = getEnvVarOrThrow("GH_WORKFLOW_ACTOR");
 const packageFile = getEnvVarOrThrow("PACKAGE_FILE");
 const bumpKind = getEnvVarOrThrow("RELEASE_KIND") as ReleaseType;
+
+if (!fileExists(packageFile)) {
+  throw new Error(
+    `packageFile: ${packageFile} not exists`,
+  );
+}
 
 const packageInfo = JSON.parse(await Deno.readTextFile(packageFile)) as {
   version: string;
@@ -43,8 +51,12 @@ const originalBranch = await repo.gitCurrentBranch();
 
 // Create and push branch
 
-const releaseBranchTitle = `chore/bot/update-${packageInfo.name}-version`;
-const commitMessage = (await isMonorepoProject())
+const monorepoProject = await isMonorepoProject();
+
+const releaseBranchTitle = monorepoProject
+  ? `release/${packageInfo.name}/${newVersion}`
+  : `release/${newVersion}`;
+const commitMessage = monorepoProject
   ? `chore(${packageInfo.name}): update ${packageInfo.name} version to ${newVersion}`
   : `chore: update ${packageInfo.name} version to ${newVersion}`;
 
@@ -68,6 +80,15 @@ const openedPr = await octoKit.request("POST /repos/{owner}/{repo}/pulls", {
   head: releaseBranchTitle,
   draft: true,
   title: commitMessage,
+  body: `Bumped ${packageInfo.name} version
+Please ensure:
+- [ ] Target branch is correct (\`main\`)
+- [ ] \`${packageInfo.name}\` version is bumped correctly
+To make edits to this PR:
+\`\`\`shell
+git fetch origin ${releaseBranchTitle} && git checkout -b ${releaseBranchTitle} origin/${releaseBranchTitle}
+\`\`\`
+ncc @${actor}`,
 });
 
-$.log(`Opened PR at ${openedPr.data.url}`);
+$.log(`Opened PR at ${openedPr.data.html_url}`);
--- denoified-actions/frontend/utils.ts
+++ denoified-actions/frontend/utils.ts
@@ -1,19 +1,30 @@
-import { fileExists } from '../fundamental/utils.ts'
-import { join } from 'jsr:@std/path@^0.221.0'
+import { fileExists } from "../fundamental/utils.ts";
+import { join } from "@std/path";
+import { parse } from "@std/yaml";
 
 export async function isMonorepoProject() {
-  const rootPath = Deno.cwd()
+  const rootPath = Deno.cwd();
 
-  if (await fileExists(join(rootPath, 'pnpm-workspace.yaml'))) {
-    return true
+  const pnpmWorkspaceYamlPath = join(rootPath, "pnpm-workspace.yaml");
+  if (await fileExists(pnpmWorkspaceYamlPath)) {
+    const content = parse(pnpmWorkspaceYamlPath) as {
+      packages?: Array<string>;
+    };
+
+    if (content.packages && content.packages.length) {
+      return true;
+    }
   }
 
-  const rootPackageJsonPath = join(rootPath, 'package.json')
-  if (await fileExists(join(rootPath, 'package.json'))) {
-    const packageInfo = JSON.parse(await Deno.readTextFile(rootPackageJsonPath))
+  const rootPackageJsonPath = join(rootPath, "package.json");
+  if (await fileExists(join(rootPath, "package.json"))) {
+    const packageInfo = JSON.parse(
+      await Deno.readTextFile(rootPackageJsonPath),
+    );
 
-    return Array.isArray(packageInfo.workspaces) && packageInfo.workspaces.length
+    return Array.isArray(packageInfo.workspaces) &&
+      packageInfo.workspaces.length;
   }
 
-  return false
+  return false;
 }
